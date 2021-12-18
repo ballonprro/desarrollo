dfff34c0a6ed396af16029a15a5f41bd53980fc9
Victor75824660 <75824660@certus.edu.pe>
2021-12-17 21:24:23 -0500

    confirmar

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,23 @@
 //ESTE SERA NUESTRO ENRUTADOR - Recibe y envia respuestas
 const { render } = require('ejs');//Va importar de la libreria ejs
 const fs =require('fs')//Importar desde la libreria fs "sistema de archivos"
-const express = require('express')//Servidor de paginas
+const express = require('express');//Servidor de paginas
+const { mongoose } = require('mongoose');
 const router = express.Router() // Traemos el enrutador del servidor express
 const client = require('../libs/connect')()//Traemos la funcion de coneccion de base de datos
+
 
 module.exports = router; //Exportamos el ruteador
 
 router.get('/inicio' ,(req, res) => {
-    res.render('index')
+    res.render('inicio')
 })
 
 router.get('/uci' ,(req, res) => {
     res.render('UCI')
+})
+router.get('/pago' ,(req, res) => {
+    res.render('pago')
 })
 
 router.post('/prueba' , (req, res)=> { //ENVIAR INFORMACION A LA BASE DE DATOS
@@ -26,8 +31,6 @@ router.post('/prueba' , (req, res)=> { //ENVIAR INFORMACION A LA BASE DE DATOS
         }
     })
 })
-
-
 
 
 router.get('/prueba2' , (req, res ) =>{ //Expones el index
@@ -55,6 +58,7 @@ router.post('/prueba2' , (req, res)=> { //TRAER INFORMACION DE LA BASE DE DATOS
         }
     })
 })
+
 router.post('/ucinorte' , (req, res)=> { //TRAER INFORMACION DE LA BASE DE DATOS
     client.connect(async (err) =>{
         if(!err){ //Preguntamos si no hay error
@@ -95,21 +99,21 @@ router.get('/ucisur' , (req, res ) =>{ //Expones el index
     res.render('ucisur')
     
 })
-router.post('/ucicentro' , (req, res)=> { //TRAER INFORMACION DE LA BASE DE DATOS
-    client.connect(async (err) =>{
-        if(!err){ //Preguntamos si no hay error
-            const collection = client.db("uci").collection("limacentro") //Conectamos a la base de datos y ala coleccion
-            collection.find().toArray((err, result) => {
-                if(!err){
-                    res.render('ucicentro' , {datos:result})
-                }else{
-                    res.send("resultado: [{'respuesta' : 'Error al traer la data'}, {'mensaje' : " + err+ "}]")
-                }
-            })
-        }else{
-            res.send("resultado: [{'respuesta' : 'Error al traer la data'}, {'mensaje' : " + err+ "}]")
-        }
-    })
+router.post('/ucicentro', (req, res) => {
+        client.connect(async (err) =>{
+            if(!err){
+                const collection = client.db("uci").collection("limacentro")
+                collection.find().toArray((err, result) =>{
+                    if(!err){
+                        res.render('ucicentro' ,{dato : result})
+                    }else{
+                        res.send("resultado: [{'respuesta' : 'Error al traer la data'}, {'mensaje' : " + err+ "}]")
+                    }
+                })
+            }else{
+                res.send("resultado: [{'respuesta' : 'Error al traer la data'}, {'mensaje' : " + err+ "}]")
+            }
+        })
 })
 router.get('/ucicentro' , (req, res ) =>{ //Expones el index
     res.render('ucicentro')
@@ -175,6 +179,5 @@ router.get('/ponorte' , (req, res ) =>{ //Expones el index
     res.render('ponorte')
     
 })
-
 
 module.exports = router;
